eb9c1e333793e1702a295bbdd8764b4dba0f821a
MarlonVictor <cmarlonvictor11@gmail.com>
2020-04-19 11:20:37 -0300

    0.5

--- frontend/script.js
+++ frontend/script.js
@@ -1,8 +1,20 @@
 // btn de cadastro
 
-function onOff(){
+function onOff() {
     document
         .querySelector("#modal")
         .classList
         .toggle("hide")
+}
+
+// btn de entrar
+
+function Login() {
+    var done = 0;
+    var username = document.login.username.value;
+    username = username.toLowerCase();
+    var password = document.login.password.value;
+    password = password.toLowerCase();
+    if (username == "danilo" && password == "marlon") { window.location = "inicio.html"; done = 1; }
+    if (done == 0) { alert("Senha ou Usuário inválido."); }
 }
