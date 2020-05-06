27e3d6780c5affd9623ffe23aa51fbc2d2c090d3
MarlonVictor <cmarlonvictor11@gmail.com>
2020-05-06 19:17:59 -0300

    0.7

--- frontend/script.js
+++ frontend/script.js
@@ -52,3 +52,22 @@ function change() {
     sentido.innerHTML = "Sentido: Saracuruna (20 pontos)"
   }
 }
+
+// Account
+
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
+})
